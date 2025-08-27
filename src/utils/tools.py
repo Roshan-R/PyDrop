e4598719804ab4522ece2dacc0ea7effe0e05e43
Roshan-R <roshanr2001@gmail.com>
2025-08-27 14:37:56 +0530

    feat(ui): create overlayed image paintables

--- src/utils/tools.py
+++ src/utils/tools.py
@@ -5,7 +5,8 @@ import gi
 gi.require_version("Gtk", "4.0")  # noqa
 from gi.repository import Gio, Gtk, Gdk, GLib  # noqa
 from gi.repository.GdkPixbuf import Pixbuf
-from gi.repository.GLib import GError
+from gi.repository.Graphene import Point
+from gi.repository.Gdk import Texture
 
 
 # Instead of hardcoding the value, get it from the image_widget
@@ -29,32 +30,61 @@ def get_desktop(link):
     return f"[Desktop Entry]\nEncoding=UTF-8\nType=Link\nURL={link}\nIcon=text-html"
 
 
+def create_overlayed_paintable(current_p, new_p):
+    # FIXME: normal images get stretched
+    width, height = pixbuf_size, pixbuf_size
+    snapshot = Gtk.Snapshot.new()
+
+    if current_p:
+        snapshot.translate(Point().alloc().init(75, 75))
+        snapshot.rotate(-5)
+        snapshot.translate(Point().alloc().init(-75, -75))
+        width = current_p.get_intrinsic_width()
+        height = current_p.get_intrinsic_height()
+        current_p.snapshot(snapshot, width, height)
+
+    new_p.snapshot(snapshot, width, height)
+
+    return snapshot.to_paintable()
+
+
 def set_image(link_stack, image_widget, mime_type=None):
     """
     Sets an appropriate image or icon on the provided image widget based on
     the MIME type or file content.
     """
+    current_paintable = image_widget.get_paintable()
+    # TODO: cannot get correct icon for .desktop files
     if mime_type:
-        image_widget.set_from_gicon(Gio.content_type_get_icon(mime_type))
-        return
+        icon = Gio.content_type_get_icon(mime_type)
+        paintable = get_paintable_from_gicon(icon)
+
     file_path = unquote(link_stack[-1])
     file = Gio.File.new_for_path(file_path)
     info = file.query_info(
         ",".join(
-            [Gio.FILE_ATTRIBUTE_STANDARD_ICON, Gio.FILE_ATTRIBUTE_STANDARD_CONTENT_TYPE]
+            [Gio.FILE_ATTRIBUTE_STANDARD_ICON,
+                Gio.FILE_ATTRIBUTE_STANDARD_CONTENT_TYPE]
         ),
         Gio.FileQueryInfoFlags.NONE,
         None,
     )
     content_type = info.get_content_type()
+
+    # TODO: does not work in case of some files, like psd
+    # Create a fallback icon
     if content_type.split("/")[0] == "image":
         pixbuf = Pixbuf.new_from_file_at_scale(
             file_path, pixbuf_size, pixbuf_size, True
         )
-        image_widget.set_from_pixbuf(pixbuf)
+        texture = Texture.new_for_pixbuf(pixbuf)
+        paintable = texture.get_current_image()
     else:
         icon = info.get_attribute_object(Gio.FILE_ATTRIBUTE_STANDARD_ICON)
-        image_widget.set_from_gicon(icon)
+        paintable = get_paintable_from_gicon(icon)
+
+    final_paintable = create_overlayed_paintable(current_paintable, paintable)
+    image_widget.set_from_paintable(final_paintable)
 
 
 def generate_file_path(count, extension):
